018c1430a9bc0094d176ffd60293eafcb946b64a
Adam Brown <acourtneybrown@gmail.com>
2026-04-18 11:07:41 -0700

    Take 2 at `on_*`

--- Formula/zapp.rb
+++ Formula/zapp.rb
@@ -5,15 +5,21 @@ class Zapp < Formula
   version "1.0.0"
   license "MIT"
 
-  on_arm do
-    sha256 "62a84af916b4689b79b41dd98cca4166feb4f4470ee9542f06f44137456f4577"
+  on_mac do
+    on_arm do
+      sha256 "62a84af916b4689b79b41dd98cca4166feb4f4470ee9542f06f44137456f4577"
+    end
+    on_linux do
+      sha256 "8d7723cb02045d189496cedf7d41a25309640d73beff47821fc3e6009ee44d85"
+    end
   end
 
   on_intel do
-    sha256 "8d7723cb02045d189496cedf7d41a25309640d73beff47821fc3e6009ee44d85"
-  end
-
-  on_linux do
-    sha256 "087b1a41768a1813ccb8e0a5ea079341ea01e139a248d95537f6b06ab29541d2"
+    on_mac do
+      sha256 "8d7723cb02045d189496cedf7d41a25309640d73beff47821fc3e6009ee44d85"
+    end
+    on_linux do
+      sha256 "087b1a41768a1813ccb8e0a5ea079341ea01e139a248d95537f6b06ab29541d2"
+    end
   end
 end
