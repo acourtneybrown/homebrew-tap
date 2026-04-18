2d9f1bad293454fb567b680b7a3cdc58640ac4d9
Adam Brown <acourtneybrown@gmail.com>
2026-04-18 10:39:54 -0700

    Style updates

--- Formula/zapp.rb
+++ Formula/zapp.rb
@@ -1,8 +1,8 @@
 class Zapp < Formula
-  desc "Flash ZSA keyboards from your terminal."
+  desc "Flash ZSA keyboards from your terminal"
   homepage "https://github.com/zsa/zapp"
-  version "1.0.0"
   url "https://github.com/zsa/zapp/releases/download/v#{version}/zapp-macos-aarch64.tar.gz"
+  version "1.0.0"
   sha256 "62a84af916b4689b79b41dd98cca4166feb4f4470ee9542f06f44137456f4577"
   license "MIT"
 end
