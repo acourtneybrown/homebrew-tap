85e58b7844487a8c221490e0fd1d0676a6835b85
Adam Brown <acourtneybrown@gmail.com>
2026-04-18 10:34:40 -0700

    Add formula for ZSA's `zapp` cli firmware tool

---
+++ Formula/zapp.rb
@@ -0,0 +1,11 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Zapp < Formula
+  desc "Flash ZSA keyboards from your terminal."
+  homepage "https://github.com/zsa/zapp"
+  version "1.0.0"
+  url "https://github.com/zsa/zapp/releases/download/v#{version}/zapp-macos-aarch64.tar.gz"
+  sha256 "62a84af916b4689b79b41dd98cca4166feb4f4470ee9542f06f44137456f4577"
+  license "MIT"
+end
